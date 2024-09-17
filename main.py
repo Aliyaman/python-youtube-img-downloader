9ea0a3402f64bbe7966215528c3865ed5a8a4f47
Ali Yaman <ali4yaman@gmail.com>
2024-09-17 23:31:15 +0300

    second commit

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import youtube_dl
 from selenium import webdriver
 
-#url = "https://www.youtube.com/@testotaylan"
-url = "https://www.youtube.com/watch?v=_ksoWeZdG54"
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 driver = webdriver.Firefox()
 driver.set_window_size(600,600)
 
+#All possible functions to use
+
 #youtube_dl.profile_pic_downloader(url, driver)
 #youtube_dl.banner_downloader(url,driver)
-youtube_dl.download_thumbnail(url,driver)
+#youtube_dl.download_thumbnail(url,driver)
