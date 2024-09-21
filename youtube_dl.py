6b7f5079f401ff3f0dd8b948bb7cd1c851e0739f
Ali Yaman <ali4yaman@gmail.com>
2024-09-21 20:14:02 +0300

    new feature: download community post img

--- youtube_dl.py
+++ youtube_dl.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from time import sleep
 from download_image import downloader
 
@@ -10,6 +11,7 @@ def profile_pic_downloader(url, driver):
     sleep(5)
     img_url = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/div[3]/ytd-tabbed-page-header/tp-yt-app-header-layout/div/tp-yt-app-header/div[2]/div/div[2]/yt-page-header-renderer/yt-page-header-view-model/div/div[1]/yt-decorated-avatar-view-model/yt-avatar-shape/div/div/div/img").get_attribute("src")
     downloader(img_url, file_name)
+    sleep(2)
     driver.quit()
 
 def banner_downloader(url, driver):
@@ -19,6 +21,7 @@ def banner_downloader(url, driver):
     sleep(5)
     img_url = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/div[3]/ytd-tabbed-page-header/tp-yt-app-header-layout/div/tp-yt-app-header/div[2]/div/div[1]/div/yt-image-banner-view-model/img").get_attribute("src")
     downloader(img_url, file_name)
+    sleep(2)
     driver.quit()
 
 def download_thumbnail(url, driver):
@@ -36,4 +39,67 @@ def download_thumbnail(url, driver):
     url = style_value[url_start:url_end]
     print(url)
     downloader(url, file_name)
+    sleep(2)
+    driver.quit()
+
+def download_community_post(url, driver):
+    #This funcion downloads pictures shared in Youtube community post
+    file_name = url[90:]
+    driver.get(url)
+    sleep(5)
+    img_url = driver.find_element(By.CSS_SELECTOR,"ytd-backstage-image-renderer.style-scope:nth-child(1) > div:nth-child(2) > yt-img-shadow:nth-child(1) > img:nth-child(1)").get_attribute("src")
+    downloader(img_url,file_name)
+    sleep(2)
+    print(f"Image downloaded as: {file_name}.jpg")
+    try:
+        #is there second image
+        right_arrow = driver.find_element(By.XPATH,"""//*[@id="right-arrow"]""")
+        right_arrow.click()
+        img_url = driver.find_element(By.CSS_SELECTOR,"ytd-backstage-image-renderer.style-scope:nth-child(2) > div:nth-child(2) > yt-img-shadow:nth-child(1) > img:nth-child(1)").get_attribute("src")
+        downloader(img_url,file_name+"2")
+        sleep(2)
+        print(f"Image downloaded as: {file_name}2.jpg")
+    except NoSuchElementException:
+        pass
+    except ElementNotInteractableException:
+        pass
+
+    try:
+        #is there third image
+        right_arrow = driver.find_element(By.XPATH,"""//*[@id="right-arrow"]""")
+        right_arrow.click()
+        img_url = driver.find_element(By.CSS_SELECTOR,"ytd-backstage-image-renderer.style-scope:nth-child(3) > div:nth-child(2) > yt-img-shadow:nth-child(1) > img:nth-child(1)").get_attribute("src")
+        downloader(img_url,file_name+"3")
+        sleep(2)
+        print(f"Image downloaded as: {file_name}3.jpg")
+    except NoSuchElementException:
+        pass
+    except ElementNotInteractableException:
+        pass
+
+    try:
+        #is there fourth image
+        right_arrow = driver.find_element(By.XPATH,"""//*[@id="right-arrow"]""")
+        right_arrow.click()
+        img_url = driver.find_element(By.CSS_SELECTOR,"ytd-backstage-image-renderer.style-scope:nth-child(4) > div:nth-child(2) > yt-img-shadow:nth-child(1) > img:nth-child(1)").get_attribute("src")
+        downloader(img_url,file_name+"4")
+        sleep(2)
+        print(f"Image downloaded as: {file_name}4.jpg")
+    except NoSuchElementException:
+        pass
+    except ElementNotInteractableException:
+        pass
+
+    try:
+        #is there fifth image
+        right_arrow = driver.find_element(By.XPATH,"""//*[@id="right-arrow"]""")
+        right_arrow.click()
+        img_url = driver.find_element(By.CSS_SELECTOR,"ytd-backstage-image-renderer.style-scope:nth-child(5) > div:nth-child(2) > yt-img-shadow:nth-child(1) > img:nth-child(1)").get_attribute("src")
+        downloader(img_url,file_name+"5")
+        sleep(2)
+        print(f"Image downloaded as: {file_name}5.jpg")
+    except NoSuchElementException:
+        pass
+    except ElementNotInteractableException:
+        pass
     driver.quit()
